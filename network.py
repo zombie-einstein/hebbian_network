9c2db6742bb688d5a8f1ef5f939cc578d4245107
zombie-einstein <jlanglopez@hotmail.com>
2018-02-01 21:49:35 +0000

    Clean-up genetic algorithm, update notebook

--- network.py
+++ network.py
@@ -1,28 +1,50 @@
 import numpy as np
-from scipy.special import expit as sig # Sigmoid function
+from scipy.special import expit as sig
 
-class nnet:
-    def __init__(self,learn_rate,input_structure,hidden_structure,output_structure):
-        self.lr = learn_rate
+
+class NeuralNet:
+    def __init__(self, learn_rate, input_structure, hidden_structure, output_structure):
+        self.learn_rate = learn_rate
         assert type(input_structure) == np.ndarray
         hidden_size = input_structure.shape[0]
         input_size = input_structure.shape[1]
-        self.Si = input_structure  # Input to hidden structure (Time independent)
-        assert type(hidden_structure) == np.ndarray and hidden_structure.shape == (hidden_size,hidden_size)
-        self.Sh = hidden_structure # Hidden layer structure (Time independent)
+        # Time independent structural weights
+        self.S_i = input_structure
+        assert type(hidden_structure) == np.ndarray and hidden_structure.shape == (hidden_size, hidden_size)
+        self.S_h = hidden_structure
         assert type(output_structure) == np.ndarray and output_structure.shape[1] == hidden_size
-        self.So = output_structure
-        self.vi = np.random.rand(input_size, 1)  # Input vector
-        self.vn = np.random.rand(hidden_size, 1)  # Hidden layer neurons
-        self.Wi = np.random.randint(2, size=(hidden_size, input_size))  # Input layer to hidden layer weights
-        self.Wh = np.random.rand(hidden_size, hidden_size)  # Hidden layer internal weights
-
+        self.S_o = output_structure
+        # Time dependent weights
+        self.W_i = np.random.randint(2, size=(hidden_size, input_size))
+        self.W_h = np.random.rand(hidden_size, hidden_size)
+        # Neuron vectors
+        self.v_i = np.random.rand(input_size, 1)
+        self.v_h = np.random.rand(hidden_size, 1)
+        
     def update(self):
-        "One update step of network, update outputs and weightings"
-        o = sig(np.multiply(np.concatenate((self.Wi,self.Wh),axis=1),np.concatenate((self.Si,self.Sh),axis=1)).dot(np.concatenate((self.vi,self.vn),axis=0)))-0.5
-        dt = self.vn-o  # Change in neuron output
-        self.Wh = sig(self.Wh+self.lr*dt.T*dt)-0.5  # Update weights of hidden layer
-        self.vn = o
+        """One update step of network, update outputs and weightings"""
+        # Updated Neuron activity based on current state
+        hidden_update = np.tanh(np.multiply(np.concatenate((self.W_i, self.W_h), axis=1), np.concatenate((self.S_i, self.S_h), axis=1)).dot(np.concatenate((self.v_i, self.v_h), axis=0)))
+        # Change in neuron activity between time-steps
+        dt = hidden_update - self.v_h
+        # Update weights of hidden layer based on change
+        self.W_h = self.W_h + np.tanh(self.learn_rate * dt.T * dt)
+        # Update current state
+        self.v_h = hidden_update
 
     def read_outputs(self):
-        return sig(self.So.dot(self.vn))
+        """Read outputs as read by hidden layer"""
+        return sig(self.S_o.dot(self.v_h))
+    
+    def evaluate(self, training_set):
+        """Given a labelled training set of the form [inputs...,label] run the network and evaluate the label"""
+        score = 0.0
+        for i in training_set:
+            self.v_i = np.array(i[:-1]).reshape(len(i) - 1, 1)
+            for j in xrange(100):
+                print self.read_outputs()
+                self.update()
+            if np.argmax(self.read_outputs()) == i[-1]:
+                score += 1.0
+        return score/len(training_set)
+    
